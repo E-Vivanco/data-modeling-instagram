6e3d690deb778fb6fe190743e08a0a9b97c90016
E-Vivanco <vivanco.elsa@gmail.com>
2023-03-13 18:21:23 +0000

    ajustes 1°

--- src/models.py
+++ src/models.py
@@ -17,6 +17,9 @@ class User(Base):
     password= Column(String, nullable=False)
     email= Column(String, nullable=False)
     posts = relationship('Post',back_populates='author')
+    comments = relationship('Comment',back_populates='author')
+    followers= relationship('Follower', back_populates='following')
+    following= relationship('Follower', back_populates='follower')
     
 class Post(Base):
     __tablename__ = 'post'
@@ -24,12 +27,34 @@ class Post(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     descripcion = Column(String, nullable=False)
-    post_code = Column(String(250), nullable=False)
+    media = relationship('Media',back_populates='post')
     author_id = Column(Integer, ForeignKey('user.id'))
     author = relationship('User',back_populates='posts')
+    comments = relationship('Comment', back_populates='post')
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__='comment'
+    id = Column(Integer, primary_key= True)
+    descripcion= Column(String, nullable=False)
+    author_id= Column(Integer, ForeignKey('user.id'))
+    post_id= Column(Integer, ForeignKey('post.id'))
+    author= relationship('User',back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+class Media(Base):
+    __tablename__='media'
+    id= Column(Integer, primary_key=True)
+    url= Column(String, nullable= False)
+    post_id=Column(Integer,ForeignKey('post.id'))
+    post = relationship('Post',back_populates='media')
+
+class Follower(Base):
+    __tablename__='follower'
+    id= Column(Integer,primary_key=True)
+    follower_id= Column(Integer,ForeignKey('user.id'))
+    follower= relationship('User', back_populates='following', foreign_keys=[follower_id])
+    following_id= Column(Integer,ForeignKey('user.id'))
+    following= relationship('User',back_populates='followers', foreign_keys=[following_id])
 
 ## Draw from SQLAlchemy base
 try:
